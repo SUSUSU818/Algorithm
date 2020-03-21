2acce3ca869d3476e2a56b49c01c8ef018da8866
SUSUSU818 <52225122+SUSUSU818@users.noreply.github.com>
2020-03-21 14:04:32 +0800

    Update Binary mergeSort.cpp

--- homework_4/Binary mergeSort.cpp
+++ homework_4/Binary mergeSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void merge(int a[],int left, int mid, int right,int temp[]) {
-    int startA = left, startB = mid + 1,pos=0;        //startA、startB表示要归并的两段数组的开始下标  pos记录temp数组中的下标
+    int startA = left, startB = mid + 1,pos=1;        //startA、startB表示要归并的两段数组的开始下标  pos记录temp数组中的下标
     while (startA <= mid && startB <= right) {
         if (a[startA] < a[startB])                    //较小值存入temp数组
             temp[pos++]=a[startA++];
@@ -17,7 +17,7 @@ void merge(int a[],int left, int mid, int right,int temp[]) {
         temp[pos++] = a[startB++];
 
     //更新原数组为归并后内容
-    pos = 0;                                          //注意temp只存储了当前进行归并排序的数据
+    pos = 1;                                          //注意temp只存储了当前进行归并排序的数据
     for (int i = left; i <= right; i++)
         a[i] = temp[pos++];
 }
@@ -33,16 +33,17 @@ void mergeSort(int a[], int left, int right,int temp[]) {
 
 int main()
 {
-    int n = 8, a[8] = { 5,9,6,3,5,2,7,1 }, temp[8] = { 0 };
+    //下标从1开始
+    int n = 8, a[9] = { 0,5,9,6,3,5,2,7,1 }, temp[9] = { 0 };
 
     cout << "排序前：";
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         cout << a[i] << " ";
     cout << endl;
 
-    mergeSort(a,0,n-1,temp);
+    mergeSort(a,1,n,temp);
 
     cout << "排序后：";
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         cout << a[i] <<" ";
 }
