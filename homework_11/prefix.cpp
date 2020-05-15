8fb58b8f8ef6de4e013424c4ac6f354c4d65237b
SUSUSU818 <52225122+SUSUSU818@users.noreply.github.com>
2020-05-15 22:56:58 +0800

    Update prefix.cpp

--- homework_11/prefix.cpp
+++ homework_11/prefix.cpp
@@ -3,63 +3,60 @@
 #include <cstring>
 #include <cstdio>
 using namespace std;
-#define maxSize 100
+const int maxSize = 100;
 
 typedef struct Node
 {
-	int weight;       
-	int parent;            
-	int lchild, rchild;   
-}HTNode, * HuffmanTree;  
-typedef char** HuffmanCode;  
+	int weight,parent,lchild, rchild;   
+}HTNode, * HfTree;  
+typedef char** HfCode;  
 
-HuffmanTree create_HuffmanTree(int* w, int n);
-void select_min(HuffmanTree HT, int k, int& min1, int& min2);
-int min(HuffmanTree HT, int k);
-void HuffmanCoding(HuffmanTree HT, HuffmanCode& HC, int n);
+HfTree create_HfTree(int* w, int n);
+void select(HfTree HT, int k, int& min1, int& min2);
+int min(HfTree HT, int k);
+void HfCoding(HfTree HT, HfCode& HC, int n);
+
 int main()
 {
-	int n=5,w[] = { 35,10,20,20,15 };
+	//初始化
+	int n=8,w[] = { 5,5,10,10,10,15,20,25 };//w为每个字符的权重
+	char s[] = "abcdefgh";
+	HfCode HC = NULL;
+	HfTree hTree = create_HfTree(w, n);
 
-	HuffmanCode HC = NULL;
-	HuffmanTree hTree = create_HuffmanTree(w, n);
+	//调用哈夫曼编码
+	HfCoding(hTree, HC, n);
 
+	//输出字符对应的编码
 	printf("从根结点到叶子结点编码结果为：\n");
-	HuffmanCoding(hTree, HC, n);
+	for (int i = 0; i < n; ++i) {
+		printf("%c—%s\n",s[i], HC[i]);
+	}
 }
 
-HuffmanTree create_HuffmanTree(int* w, int n)
+//构造哈夫曼树
+HfTree create_HfTree(int* w, int n)
 {
 	int total = 2 * n - 1;
-	HuffmanTree HT = (HuffmanTree)malloc(total * sizeof(HTNode));
-	if (!HT)
-	{
-		printf("HuffmanTree malloc faild!");
-		exit(-1);
-	}
-	int i;
+	HfTree HT = (HfTree)malloc(total * sizeof(HTNode));
 
+	int i;
 	for (i = 0; i < n; i++)
 	{
-		HT[i].parent = -1;
-		HT[i].lchild = -1;
-		HT[i].rchild = -1;
+		HT[i].parent =HT[i].lchild = HT[i].rchild = -1;
 		HT[i].weight = *w;
 		w++;
 	}
- 
 	for (; i < total; i++)
 	{
-		HT[i].parent = -1;
-		HT[i].lchild = -1;
-		HT[i].rchild = -1;
+		HT[i].parent = HT[i].lchild = HT[i].rchild = -1;
 		HT[i].weight = 0;
 	}
 
-	int min1, min2;
+	int min1, min2;//用于保存每次循环选出的两个weight最小且parent为0的节点
 	for (i = n; i < total; i++)
 	{
-		select_min(HT, i, min1, min2);
+		select(HT, i, min1, min2);
 		HT[min1].parent = i;
 		HT[min2].parent = i;
 		HT[i].lchild = min1;
@@ -69,38 +66,41 @@ HuffmanTree create_HuffmanTree(int* w, int n)
 	return HT;
 }
 
-void select_min(HuffmanTree HT, int k, int& min1, int& min2)
+//从HT的前k个元素中选出两个weight最小且parent为-1的节点
+void select(HfTree HT, int k, int& min1, int& min2)
 {
 	min1 = min(HT, k);
 	min2 = min(HT, k);
 }
 
-int min(HuffmanTree HT, int k)
+//从HT的前k个元素中选出weight最小且parent为-1的节点，返回其下标
+int min(HfTree HT, int k)
 {
-	int i = 0,min,min_weight;
+	int i = 0,mini,min_weight;
   
 	while (HT[i].parent != -1)
 		i++;
 	min_weight = HT[i].weight;
-	min = i;
+	mini = i;
  
 	for (; i < k; i++)
 	{
 		if (HT[i].weight < min_weight && HT[i].parent == -1)
 		{
 			min_weight = HT[i].weight;
-			min = i;
+			mini = i;
 		}
 	}
  
-	HT[min].parent = 1;
+	HT[mini].parent = 1;
 
-	return min;
+	return mini;
 }
 
-void HuffmanCoding(HuffmanTree HT, HuffmanCode& HC, int n)
+//求哈夫曼编码
+void HfCoding(HfTree HT, HfCode& HC, int n)
 {
-	HC = (HuffmanCode)malloc(n * sizeof(char*));
+	HC = (HfCode)malloc(n * sizeof(char*));
 	char* code = (char*)malloc(n * sizeof(char));
 	int cur = 2 * n - 2,code_len = 0;  
 
@@ -146,10 +146,6 @@ void HuffmanCoding(HuffmanTree HT, HuffmanCode& HC, int n)
 			cur = HT[cur].parent;
 			--code_len;
 		}
-
-	}
-	for (int i = 0; i < n; ++i) {
-		printf("%s\n", HC[i]);
 	}
 	free(code);
 }
